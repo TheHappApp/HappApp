b6ca1ff9de7bb3ad89424580f54d054b84ef0d9a
turner-white <turnerwhite@me.com>
2022-09-27 13:33:47 -0400

    Initialization

---
+++ HappApp/HappApp/HappAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappAppApp.swift
+//  HappApp
+//
+//  Created by Turner White on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct HappAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
